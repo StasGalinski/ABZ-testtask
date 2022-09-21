819113a2d065dbbd2eca917d31a53c13bb5c7039
StasGalinski <102591398+StasGalinski@users.noreply.github.com>
2022-09-21 17:24:07 +0300

    Revert "Show more Button"
    This reverts commit 03354df4dd37a939b57415df0e66856706efa404.

--- src/components/GetRequestPart.js
+++ src/components/GetRequestPart.js
@@ -2,32 +2,20 @@ import { useState, useCallback, useEffect } from "react";
 import User from "./User";
 import classes from './GetRequestPart.module.css'
 const GetRequestPart = () => {
-    const [page,setPage] = useState(1)
     const [isLoading, setIsLoading] = useState(true);
     const [loadedUsers, setLoadedUsers] = useState([])
     const fetchUsers = useCallback(() => {
-        setIsLoading(true);
-        console.log("fetching")
-        return fetch(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`)
+        setIsLoading(true)
+        fetch('https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=5')
             .then(res => res.json())
             .then(data => {
-                setIsLoading(false);
-                return data.users;
+                console.log(data.users)
+                setLoadedUsers(data.users)
+                setIsLoading(false)
             })
-    }, [page]);
-    const nextPage = ()=>{
-        setPage(prev=> prev+1);
-    }
+    }, []);
     useEffect(() => {
-        const timer = setTimeout(()=>{
-            fetchUsers()
-            .then(data=>{
-                setLoadedUsers(prev=>[...prev,...data]);
-            })
-        },500)
-        return ()=>{
-            clearTimeout(timer)
-        }
+        fetchUsers();
     }, [fetchUsers])
     let content;
     if (loadedUsers) {
@@ -39,7 +27,7 @@ const GetRequestPart = () => {
             {isLoading && <p>Loading</p>}
             
                 {!isLoading && <div className={classes.container}> {content }</div>}
-                <button onClick={nextPage}>Show More</button>
+            
 
         </div>
     )
