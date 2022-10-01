8dcdd90c091912fe1ad5e3fb97a7fb8964409760
StasGalinski <stas.galinski@gmail.com>
2022-10-01 22:32:51 +0300

    Little fix

--- src/components/GetRequestPart.js
+++ src/components/GetRequestPart.js
@@ -3,15 +3,15 @@ import User from "./User";
 import classes from './GetRequestPart.module.css'
 const GetRequestPart = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const [loadedUsers, setLoadedUsers] = useState([])
+    const [loadedUsers, setLoadedUsers] = useState([]);
     const fetchUsers = useCallback(() => {
         setIsLoading(true)
         fetch('https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=5')
             .then(res => res.json())
             .then(data => {
-                console.log(data.users)
-                setLoadedUsers(data.users)
-                setIsLoading(false)
+                console.log(data.users);
+                setLoadedUsers(data.users);
+                setIsLoading(false);
             })
     }, []);
     useEffect(() => {
